87ff1c1f760ddb9bcd2976d962e3ea384012b23a
Miguel Ferreira dos Santos Moraes <141457865+MiguelRobotics@users.noreply.github.com>
2025-03-05 11:29:16 -0300

    Chat bot
    This is my chat bot, programmed with Python language, it interacts with the user with conditional responses. It is obviously limited, but the project reflects my skill in using syntax.

---
+++ chat bot project.py
@@ -0,0 +1,13 @@
+name = input("Hello! What is your name?")
+print(f"Nice to meet you, {name}!")
+age_input= input ("How old are you ?")
+age= int(age_input)
+bot_age = 3
+age_difference= age - bot_age
+print(f"You are {age_difference} older than me. I'm only {bot_age} years old!")
+color = input ("What's your favorite color?")
+print(f"Oh, {color} is a beautiful color!")
+dream= input("Now, talk to me about your dream")
+print (f"{name}! what a awesome dream!")
+print ("My cretaor has a dream to be a greating programming")
+      
